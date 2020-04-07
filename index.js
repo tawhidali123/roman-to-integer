9bb9dfd82b9d6851493f67c15ca8a47495dc3441
Tawhid Ali <tawhidali126@gmail.com>
2020-04-07 01:22:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var romanToInt = function(roman) {
+  const ROMAN_TABLE = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+  let temp = 0
+  let result = 0
+  for (let elem of roman) {
+    if (temp < ROMAN_TABLE[elem]) {
+      result -= temp
+      temp = ROMAN_TABLE[elem]
+    } else if (temp > ROMAN_TABLE[elem]) {
+      result += temp
+      temp = ROMAN_TABLE[elem]
+    } else {
+      temp += ROMAN_TABLE[elem]
+    }
+  }
+
+  return result + temp
+};
+
+romanToInt('CI')
